ee748275c1b1514ee1f78b1f81b9f9258df19717
Simon Nishi McCorkindale <simon@funx.jp>
2021-05-10 23:16:07 +0800

    Fix javascript channel sender case enum from gid to entropy

--- ios/Runner/TrueEntropy/GenerationController.swift
+++ ios/Runner/TrueEntropy/GenerationController.swift
@@ -369,7 +369,7 @@ class GenerationController: UIViewController, CameraFramesDelegate, UITableViewD
         
         // by soliax
         // go back to flutter
-        self.channel?.invokeMethod("gid", arguments: "Cancel")
+        self.channel?.invokeMethod("entropy", arguments: "Cancel")
         self.coordinatorDelegate?.navigateToFlutter()
     }
 }
